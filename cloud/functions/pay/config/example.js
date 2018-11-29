5a27dae0bb90ec9a524df3b573bde2c86de4332d
heyli <lcxfs1991@gmail.com>
2018-11-29 15:51:13 +0800

    Update example.js

--- cloud/functions/pay/config/example.js
+++ cloud/functions/pay/config/example.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   ENV: 'xxxxxx-xxxxxx', // TCB环境ID
   MCHID: 'xxxxxxxxx',//商户id
   KEY: 'xxxxxx',
-  //   CERT_FILE_CONTENT: fs.readFileSync('/path/to/apiclient_cert.p12'),
+  //   CERT_FILE_CONTENT: fs.readFileSync(path.join(__dirname, './apiclient_cert.p12')),
   TIMEOUT: 10000 // 毫秒
 };
