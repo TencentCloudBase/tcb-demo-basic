298fcd5f4c91572088aae01a7de6bd73f716228a
yang <yhsunshining@gmail.com>
2025-02-10 14:55:44 +0800

    fix: 修正 init sdk 时机

--- client/app.js
+++ client/app.js
@@ -1,11 +1,4 @@
-require.async('./components/agent-deepseek/index.js').then(module=>{
-  module.clientSDK.init({
-    envID: 'tcb-advanced-a656fc', // 云开发环境Id
-  appConfig: {
-    staticResourceDomain: 'tcb-advanced-a656fc-1257967285.tcloudbaseapp.com', // 云开发环境下静态托管域名，用于使用素材资源
-  }
-  })
-})
+require.async('./components/agent-deepseek/index.js')
 //app.js
 App({
   onLaunch: function () {
--- client/components/agent-deepseek/index.js
+++ client/components/agent-deepseek/index.js
@@ -1,1 +1,8 @@
 export * as clientSDK from '@cloudbase/weda-client';
+
+clientSDK.init({
+  envID: 'tcb-advanced-a656fc', // 云开发环境Id
+  appConfig: {
+    staticResourceDomain: 'tcb-advanced-a656fc-1257967285.tcloudbaseapp.com', // 云开发环境下静态托管域名，用于使用素材资源
+  }
+})
